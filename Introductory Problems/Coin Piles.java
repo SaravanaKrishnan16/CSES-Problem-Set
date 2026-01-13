dec26934086203691c077ed7ee5a57be2b6cd617
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2026-01-13 12:46:49 +0530

    Introductory Problem

---
+++ Introductory Problems/Coin Piles.java
@@ -0,0 +1,50 @@
+You have two coin piles containing a and b coins. On each move, you can either remove one coin from the left pile and two coins from the right pile, or two coins from the left pile and one coin from the right pile.
+Your task is to efficiently find out if you can empty both the piles.
+Input
+The first input line has an integer t: the number of tests.
+After this, there are t lines, each of which has two integers a and b: the numbers of coins in the piles.
+Output
+For each test, print "YES" if you can empty the piles and "NO" otherwise.
+Constraints
+
+1 \le t \le 10^5
+0 \le a, b \le 10^9
+
+Example
+Input:
+3
+2 1
+2 2
+3 3
+
+Output:
+YES
+NO
+YES
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.io.*;
+import java.util.StringTokenizer;
+ 
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            long min = Math.min(a, b);
+            long max = Math.max(a, b);
+            if((a+b)%3==0 && max<=2*min){
+                bw.write("YES\n");
+            } 
+            else {
+                bw.write("NO\n");
+            }
+        }
+        bw.flush();
+    }
+}
